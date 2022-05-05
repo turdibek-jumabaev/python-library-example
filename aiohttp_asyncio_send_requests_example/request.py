02d6d04554ae67a6ae988dc404772a4f63d394b3
Turdibek <turdibekdev05@gmail.com>
2022-05-05 23:31:41 +0500

    Add example: aiohttp, asyncio, pprint

---
+++ aiohttp_asyncio_send_requests_example/request.py
@@ -0,0 +1,16 @@
+import aiohttp  # pip install aiohttp
+import asyncio  # o'rnatish shart emas
+import pprint  # pip install pprint
+
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        # so'rov jo'natish kerak bo'lgan manzil
+        url = 'https://forbes400.herokuapp.com/api/forbes400'
+        async with session.get(url) as resp:
+            response = await resp.json()
+            # natijani console-ga chiroyli chiqarish uchun
+            pprint.pprint(response)
+
+asyncio.run(main())
